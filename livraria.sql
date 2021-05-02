81109f3c5da1f7a3e2e526df1a8f6a27bfcaf06f
William B. Pereira <filmileirodevop@gmail.com>
2021-05-02 03:55:47 -0300

    Atualização de arquivos

--- livraria.sql
+++ livraria.sql
@@ -1,21 +1,18 @@
 /*
  
- O nosso cliente solicitou uma tabela para armazenar livros são comercializados pela empresa.
- A solicitação é somente para livros e não há a necessidade de realizar busca em outras tabelas.
- Hoje há um funcionário de vendas que tem uma tabela do Excel para guardar esses registros,
- mas as buscas estão ficando complexas. Decidiu-se então criar um banco de dados separado
- para esse funcionário.
- 
- Após a criação da tabela, devemos integrar algumas queries prontas que sejam enviadas para o programador.
- As queries. são as seguintes:
- 
- 1- Trazer os todos os dados.
- 2- Trazer O nome do livro e o nome da editora.
- 3- Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino.
- 4- Trazer o nome do livro e o numero de páginas do livro publicados por autores do sexo feminino.
- 5- Trazer os valores dos livros das editoras de São Paulo.
- 6- Trazer os dados dos autores do sexo masculIno que tiveram livros publicados por Paulo ou Rio de Janeiro (questão desafio).
- 
+O nosso cliente solicitou uma tabela para armazenar os livros que são comercializados pela empresa. A solicitação é 
+somente para livros e não há a necessidade de realizar busca em outras tabelas. Hoje há um funcionário de vendas que 
+tem uma tabela do Excel para guardar esses registros, mas as buscas estão ficando complexas. Decidiu-se então criar 
+um banco de dados separado para esse funcionário.
+Após a criação da tabela, deveremos entregar algumas queries prontas para que sejam enviadas para o programador. 
+
+As queries são as seguintes:
+1 – Trazer todos os dados.
+2 – Trazer o nome do livro e o nome da editora
+3 – Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino.
+4 - Trazer o nome do livro e o número de páginas dos livros publicados por autores do sexo feminino.
+5 – Trazer os valores dos livros das editoras de São Paulo.
+6 – Trazer os dados dos autores do sexo masculino que tiveram livros publicados por São Paulo ou Rio de Janeiro (Questão Desafio).
  
  */
 /*CRIANDO O BANCO DE DADOS LIVRARIA*/
@@ -25,76 +22,175 @@ CREATE DATABASE LIVRARIA;
 USE LIVRARIA;
 
 /*CRIANDO TABELAS E ISERINDO ATRIBUTOS*/
-CREATE TABLE ESTOQUE(
-    LIVRO VARCHAR(100),
-    EDITORA VARCHAR (60),
-    UF CHAR(2),
-    AUTOR VARCHAR (100),
-    SEXO CHAR(1),
-    PAGINA SMALLINT,
-    VALOR FLOAT(10,2)
+CREATE TABLE LIVROS(
+    NOME_LIVRO VARCHAR(100),
+    NOME_AUTOR VARCHAR (100),
+    SEXO_AUTOR CHAR(1),
+    NUMERO_PAGINAS SMALLINT,
+    NOME_EDITORA VARCHAR (60),
+    VALOR_LIVRO FLOAT(10,2),
+    UF_EDITORA CHAR(2),
+    ANO_PUBLICACAO INT
+    
 );
 
 /*INSERINDO VALORES*/
 INSERT INTO
-    ESTOQUE (LIVRO, EDITORA, UF, AUTOR, SEXO, PAGINAS, VALOR)
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
 VALUES
 (
-        'O ALQUIMISTA',
-        'PARALELA',
-        'SP',
-        'PAULO COELHO',
-        'M',
-        208,
-        16.90
-    );
-
-
-INSERT INTO
-    ESTOQUE (LIVRO, EDITORA, UF, AUTOR, SEXO, PAGINAS, VALOR)
-VALUES
-(
-        'AMERICANAH',
-        'COMPANHIA DAS LETRAS',
-        'SP',
-        'CHIMAMANDA NGOZI ADICHIE',
+        'Cavaleiro Real',
+        'Ana Claudia',
         'F',
-        520,
-        39.90
+        465,
+        'Atlas',
+        49.90,
+        'RJ',
+        2009
+
     );
 
+
 INSERT INTO
-    ESTOQUE (LIVRO, EDITORA, UF, AUTOR, SEXO, PAGINAS, VALOR)
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
 VALUES
 (
-        'OCEANO NO FIM DO CAMINHO',
-        'INTRÍSECA',
-        'RJ',
-        'NEIL GAIMAN',
+        'SQL Para Leigos',
+        'João Nunes',
         'M',
-        208,
-        22.90
+        450,
+        'Addison',
+        98.00,
+        'SP',
+        2018
+
+    );
+
+
+INSERT INTO
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
+VALUES
+(
+        'Receitas Caseiras',
+        'Celia Tavares',
+        'F',
+        210,
+        'Atlas',
+        45.00,
+        'RJ',
+        2018
+
     );
 
 INSERT INTO
-    ESTOQUE (LIVRO, EDITORA, UF, AUTOR, SEXO, PAGINAS, VALOR)
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
 VALUES
 (
-        'MULHERES, RAÇA E CLASSE',
-        'GOITEMPO',
+        'Pessoas Afetivas',
+        'Eduardop Santos',
+        'M',
+        390,
+        'Beta',
+        78.99,
         'RJ',
-        'ANGELA DAVIS ',
-        'F',
-        248,
-        42.29
+        2018
+
     );
 
+INSERT INTO
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
+VALUES
+(
+        'Habitos Saudáveis',
+        'Eduardo Santos',
+        'M',
+        630,
+        'Beta',
+        150.98,
+        'RJ',
+        2019
+
+    );
+
+
+INSERT INTO
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
+VALUES
+(
+        'A Casa Marrom',
+        'Hermes Macedo',
+        'M',
+        250,
+        'Bubba',
+        60.00,
+        'MG',
+        2016
+
+    );
+
+INSERT INTO
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
+VALUES
+(
+        'Estacio Querido',
+        'Geraldo Francisco',
+        'M',
+        310,
+        'Insignia',
+        100.00,
+        'ES',
+        2015
+
+    );
+
+INSERT INTO
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
+VALUES
+(
+        'Pra Sempre Amigas',
+        'Leda Silva',
+        'F',
+         510,
+        'Insignia',
+        78.98,
+        'ES',
+        2011
+
+    );
+
+INSERT INTO
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
+VALUES
+(
+        'Copas Inersqueciveis',
+        'Marco Alcantara',
+        'M',
+         200,
+        'Larson',
+        130.98,
+        'RS',
+        2018
+
+    );
+
+INSERT INTO
+    LIVROS (NOME_LIVRO, NOME_AUTOR, SEXO_AUTOR, NUMERO_PAGINAS, NOME_EDITORA, VALOR_LIVRO, UF_EDITORA, ANO_PUBLICACAO)
+VALUES
+(
+        'O Poder da Mente',
+        'Clara Mafra',
+        'F',
+         120,
+        'Continental',
+        56.58,
+        'RS',
+        2017
+
+    );
 
 
 
 /*1- Trazer os todos os dados.*/
-
-USE LIVRARIA;
 
 SELECT * FROM ESTOQUE;
 
